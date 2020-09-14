4a651adad5817f64c79a9c5817d93b5d6455991b
FouedDadi <foued.dadii@gmail.com>
2020-09-14 19:11:49 +0100

    save data in csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+python script that gets data from api
+"""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    lst = []
+    id = argv[1]
+    resp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(id)).json()
+    quest = requests.get('https://jsonplaceholder.typicode.com/todos',
+                         params={'userId': id}).json()
+    name = resp.get('name')
+    with open('{}.csv'.format(id), "w") as csv_file:
+        writing = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for to in quest:
+            Title = to.get('title')
+            completion = to.get('completed')
+            writing.writerow([(id), name, completion, Title])
