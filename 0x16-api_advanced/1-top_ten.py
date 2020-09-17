1567d6460461d368a358db8ec4bfcce25636abd2
FouedDadi <foued.dadii@gmail.com>
2020-09-17 21:47:33 +0100

    hot titles for subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,14 +11,13 @@ def top_ten(subreddit):
     function to get hot titles
     """
     user = {'User-Agent': 'lycan619'}
-    request = requests.get('https://www.reddit.com/r/{}/about.json'
+    request = requests.get('https://www.reddit.com/r/{}/hot.json'
                            .format(subreddit), headers=user).json()
     if request is None:
         print('None')
     else:
-        dt = request.get('data')
-        child = dt.get('children')
-        if dt and child:
-            for data in child(limit=10):
+        child = request.get('data').get('children')
+        if child:
+            for data in child[:10]:
                 title = data.get('data').get('title')
-            print(title)
+                print(title)
